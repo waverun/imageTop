6baa0494b24523bdca57ffe95c32b89cc25bc027
Shay Moreno <waverun@me.com>
2023-05-07 20:31:46 +0300

    setting

--- ImageTop/Views/SettingsView.swift
+++ ImageTop/Views/SettingsView.swift
@@ -1,14 +1,66 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @AppStorage("inactivityDuration") private var inactivityDuration: TimeInterval = 0
+    @AppStorage("replaceImageAfter") private var replaceImageAfter: TimeInterval = 0
+    @AppStorage("selectedFolderPath") private var selectedFolderPath: String = ""
+    
     var body: some View {
         VStack {
             Text("Settings")
                 .font(.title)
                 .padding()
-            Spacer()
+            
+            GeometryReader { geometry in
+                Form {
+                    VStack {
+                        HStack {
+                            Text("Inactivity Duration")
+                                .frame(width: geometry.size.width * 0.5, alignment: .leading)
+                            TextField("", value: $inactivityDuration, formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: 50)
+                            Spacer()
+                        }.padding(.leading)
+                        HStack {
+                            Text("Replace Image After")
+                                .frame(width: geometry.size.width * 0.5, alignment: .leading)
+                            TextField("", value: $replaceImageAfter, formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: 50)
+                            Spacer()
+                        }.padding(.leading)
+                        HStack {
+                            Text("Image Folder")
+                                .frame(width: geometry.size.width * 0.5, alignment: .leading)
+                            Button("Select...") {
+                                openFolderPicker()
+                            }
+                            Spacer()
+                        }.padding(.leading)
+                        
+                        Text(selectedFolderPath)
+                            .foregroundColor(.gray)
+                            .lineLimit(1)
+                            .truncationMode(.middle)
+                    }
+                }
+            }
         }
-        .frame(width: 300, height: 200)
+        .frame(width: 235, height: 200)
+    }
+    
+    private func openFolderPicker() {
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseFiles = false
+        openPanel.canChooseDirectories = true
+        openPanel.allowsMultipleSelection = false
+        
+        openPanel.begin { result in
+            if result == .OK {
+                selectedFolderPath = openPanel.urls[0].path
+            }
+        }
     }
 }
 
